2e8d5f7a73152ac9c9f3bc3efcdea5f137a4e0be
Travis Nielsen <trniel@microsoft.com>
2022-10-11 16:40:29 -0500

    add vnets and subnets

--- scripts/generate.ps1
+++ scripts/generate.ps1
@@ -6,24 +6,35 @@ foreach ($file in $sourceFiles) {
     $dictData.add( $file.BaseName, $dataItems)
 }
 
-$diagram = Get-Content '..//templates/diagram.puml'
-$vnetTemplate = Get-Content '..//templates/vnet.puml'
-$subnetTemplate = Get-Content '..//templates/subnet.puml'
+$diagram = Get-Content '..//templates/diagram.puml' -Raw
+$vnetTemplate = Get-Content '..//templates/vnet.puml' -Raw
+$subnetTemplate = Get-Content '..//templates/subnet.puml' -Raw
+
+$diagramContent = ""
 
 # load VNETS
 foreach ($vnet in $dictData['vnets']) {
     $vnetData = $vnetTemplate
-    $subnetData = $subnetTemplate
-    $vnetData.Replace('[id]', $vnet.Name)
-    $vnetData.Replace('[name]', $vnet.Name)
-    $vnetData.Replace('[technology]', $vnet.addressSpace.addressPrefixes)
+    $vnetData = $vnetData.Replace("[id]", $vnet.Name.Replace("-", ""))
+    $vnetData = $vnetData.Replace("[name]", "`"{0}`"" -f $vnet.Name)
+    $vnetData = $vnetData.Replace("[technology]", "`"{0}`"" -f $vnet.Properties.addressSpace.addressPrefixes)
+    $vnetData = $vnetData.Replace("[description]", "`"{0}`"" -f "demo")
+    $vnetSubnets = ''
 
-    foreach ($subnet in $vnet.subnets) {
-
-
+    foreach ($subnet in $vnet.Properties.subnets) {
+        $subnetData = $subnetTemplate
+        $subnetData = $subnetData.Replace("[id]", $subnet.name.Replace("-", ""))
+        $subnetData = $subnetData.Replace("[name]", "`"{0}`"" -f $subnet.name)
+        $subnetData = $subnetData.Replace("[technology]", "`"{0}`"" -f $subnet.properties.addressPrefix)
+        $subnetData = $subnetData.Replace("[description]", "`"{0}`"" -f "demo")
+        $vnetSubnets += "`n"
+        $vnetSubnets += "`t" + $subnetData
     }
 
-    $vnetData.Replace('[subnets]', $subnetData)
+    $vnetData = $vnetData.Replace("[subnets]", $vnetSubnets)
+    $diagramContent += "`n`n"
+    $diagramContent += $vnetData
 }
 
-$diagram.Replace('[BODY]', $vnetData)
+$diagram = $diagram.Replace("[BODY]", $diagramContent)
+$diagram | Out-File "network-diag.puml"
