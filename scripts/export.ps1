b556f7806369ea4a8a42d7d4ffda002efbac5909
Travis Nielsen <trniel@microsoft.com>
2023-01-05 15:46:42 -0600

    add json configuration for other services

--- scripts/export.ps1
+++ scripts/export.ps1
@@ -1,15 +1,16 @@
-# load subscription and tenant IDs
-$contextInfo = Get-Content ./config.json | ConvertFrom-Json
+# get connection context
+$contextInfo = Get-Content ./config-v2.json | ConvertFrom-Json
+Connect-AzAccount -Tenant $contextInfo.tenantId
 $azContext = Get-AzContext
-$outFolder = $contextInfo.subscriptionId
+$outFolder = "..//data/" + $contextInfo.subscriptionName
 $listSubscriptions = New-Object -TypeName 'System.Collections.ArrayList'
 
 if ($null -eq $azContext) {
-    Connect-AzAccount -Subscription $contextInfo.subscriptionId -Tenant $contextInfo.tenantId
-    $listSubscriptions.Add($contextInfo.subscriptionId)
+    Connect-AzAccount -Subscription $contextInfo.subscriptionName -Tenant $contextInfo.tenantId
+    $listSubscriptions.Add($contextInfo.subscriptionName)
 } else {
-    Set-AzContext -Subscription $contextInfo.subscriptionId
-    $listSubscriptions.Add($contextInfo.subscriptionId)
+    Set-AzContext -Subscription $contextInfo.subscriptionName
+    $listSubscriptions.Add($contextInfo.subscriptionName)
 }
 
 # get subscription vnet info and save
@@ -35,51 +36,23 @@ foreach ($vnet in $vnets) {
 }
 
 foreach ($vnetId in $listRemoteVnetIds) {
-    $remoteNetworkSubscriptionId = $vnetId.split("/")[2]
+    $remoteNetworksubscriptionName = $vnetId.split("/")[2]
     $currentContext = Get-AzContext
 
-    if ($remoteNetworkSubscriptionId -ne $currentContext.Subscription.Id ) {
-        Set-AzContext -Subscription $remoteNetworkSubscriptionId
-        $listSubscriptions.Add($remoteNetworkSubscriptionId)
+    if ($remoteNetworksubscriptionName -ne $currentContext.Subscription.Id ) {
+        Set-AzContext -Subscription $remoteNetworksubscriptionName
+        $listSubscriptions.Add($remoteNetworksubscriptionName)
     }
 
     $remoteVnet = Get-AzResource -ResourceId $vnetId -ExpandProperties
     $listVnets.Add($remoteVnet)
 }
 
-
-# identify other subscriptions based on VNET Gateway Connection objects
-# ExpressRoute circuits are often placed in separate / isolated subscriptions
-
-$listGatewayConnections = New-Object -TypeName 'System.Collections.ArrayList'
-
-foreach ($subscription in $listSubscriptions | Select-Object -Unique) {
-    Set-AzContext -Subscription $subscription
-    Get-AzResource -ResourceType "Microsoft.Network/connections" -ExpandProperties | ForEach-Object { $listGatewayConnections.Add($_) }
-    
-    foreach ($connection in $listGatewayConnections) {
-        $connectionPeerId = $connection.properties.peer.id
-
-        # Only concerned about ER circuits in isolated subscriptions
-
-        if ($connectionPeerId) {
-            $connectionPeerSubscriptionId = $connectionPeerId.Split("/")[2]
-            if ($subscriptions -notcontains $connectionPeerSubscriptionId) {
-                $listSubscriptions.Add($connectionPeerSubscriptionId)
-            }
-        }
-    }
-}
-
-# Export objects in-scope for the identified subscriptions based on VNETs and VNET Connection objects
-
 $subscriptions = $listSubscriptions | Select-Object -Unique
 $listSubscriptionInfo = New-Object -TypeName 'System.Collections.ArrayList'
 $listRouteTables = New-Object -TypeName 'System.Collections.ArrayList'
-$listNsgs = New-Object -TypeName 'System.Collections.ArrayList'
 $listFirewalls = New-Object -TypeName 'System.Collections.ArrayList'
 $listNatGateways = New-Object -TypeName 'System.Collections.ArrayList'
-$listAppGateways = New-Object -TypeName 'System.Collections.ArrayList'
 $listVnetGateways = New-Object -TypeName 'System.Collections.ArrayList'
 $listExpressRouteCircuits = New-Object -TypeName 'System.Collections.ArrayList'
 
@@ -89,22 +62,41 @@ foreach ($subscription in $subscriptions) {
     $listSubscriptionInfo.Add($context.Subscription)
 
     Get-AzResource -ResourceType "Microsoft.Network/routeTables" -ExpandProperties | ForEach-Object { $listRouteTables.Add($_) }
-    Get-AzResource -ResourceType "Microsoft.Network/networkSecurityGroups" -ExpandProperties | ForEach-Object { $listNsgs.Add($_) }
     Get-AzResource -ResourceType "Microsoft.Network/azureFirewalls" -ExpandProperties | ForEach-Object { $listFirewalls.Add($_) }
     Get-AzResource -ResourceType "Microsoft.Network/natGateways" -ExpandProperties | ForEach-Object { $listNatGateways.Add($_) }
-    Get-AzResource -ResourceType "Microsoft.Network/applicationGateways" -ExpandProperties | ForEach-Object { $listAppGateways.Add($_) }
     Get-AzResource -ResourceType "Microsoft.Network/virtualNetworkGateways" -ExpandProperties | ForEach-Object { $listVnetGateways.Add($_) }
     Get-AzResource -ResourceType "Microsoft.Network/expressRouteCircuits" -ExpandProperties | ForEach-Object { $listExpressRouteCircuits.Add($_) }
 }
 
-New-Item -Path "..//data/${outFolder}" -ItemType Directory
+#
+# Azure services (Compute, Data, Storage, etc...)
+#
+
+Set-AzContext -Subscription $contextInfo.subscriptionName
+$services = $contextInfo.services
+$dictServices = @{}
+
+foreach ($service in $services) {
+    $items = Get-AzResource -ResourceType "${service}" -ExpandProperties
+    $dictServices.add( $service, $items)
+}
+
+#
+# Write out data
+#
+
+New-Item -Path "..//data/${outFolder}" -ItemType Directory -ErrorAction Ignore
+
 ConvertTo-Json -InputObject $listSubscriptionInfo -Depth 7 | Out-File "..//data/${outFolder}/subscriptions.json"
 ConvertTo-Json -InputObject $listVnets -Depth 7 | Out-File "..//data/${outFolder}/vnets.json"
 ConvertTo-Json -InputObject $listRouteTables -Depth 7 | Out-File "..//data/${outFolder}/routeTables.json"
-ConvertTo-Json -InputObject $listNsgs -Depth 7 | Out-File "..//data/${outFolder}/nsgs.json"
 ConvertTo-Json -InputObject $listFirewalls -Depth 7 | Out-File "..//data/${outFolder}/firewalls.json"
 ConvertTo-Json -InputObject $listNatGateways -Depth 7 | Out-File "..//data/${outFolder}/natGateways.json"
-ConvertTo-Json -InputObject $listAppGateways -Depth 7 | Out-File "..//data/${outFolder}/appGateways.json"
 ConvertTo-Json -InputObject $listVnetGateways -Depth 7 | Out-File "..//data/${outFolder}/vnetGateways.json"
 ConvertTo-Json -InputObject $listGatewayConnections -Depth 7 | Out-File "..//data/${outFolder}/connections.json"
 ConvertTo-Json -InputObject $listExpressRouteCircuits -Depth 7 | Out-File "..//data/${outFolder}/expressRouteCircuits.json"
+
+$dictServices.GetEnumerator() | ForEach-Object {
+    $filename = $_.key.Split("/")[1]
+    ConvertTo-Json -InputObject $_.value -Depth 7 | Out-File "..//data/${outFolder}/${filename}.json"
+}
